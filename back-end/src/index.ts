e961afa5761328350eadc2fdd1e9d49dee8fe838
Ahmed-M-Aboutaleb <ahmed.aboutaleb.work@gmail.com>
2025-05-15 17:09:39 +0300

    Docs: README.md

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -29,13 +29,13 @@ import { setupDeliveryPersonRoutes } from "./interfaces/http/routes/delivery-per
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 // Middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://frontend:3001", "*"],
+    origin: ["http://localhost:3000", "http://frontend:3000", "*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
